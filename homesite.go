090b7fd4897e32d6f2e893dffe0abcf52b878271
Max <max.nibler@gmail.com>
2024-01-17 14:47:33 +0900

    Basic Templates and pages

--- homesite.go
+++ homesite.go
@@ -4,22 +4,67 @@ import (
     "fmt"
     "net/http"
     "log"
+    "html/template"
+    "os"
 
     "github.com/julienschmidt/httprouter"
 )
 
+type Page struct {
+    Title string
+    Body []byte
+}
+
+func (p *Page) save() error {
+    filename := p.Title + ".txt"
+    return os.WriteFile(filename, p.Body, 0600)
+}
+
+func loadPage(title string) (*Page, error) {
+    filename := title + ".txt"
+    body, err := os.ReadFile(filename)
+    if err != nil {
+        return nil, err
+    }
+    return &Page{Title: title, Body: body}, nil
+}
+
 func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-    fmt.Fprint(w, "Welcome!\n")
+    t, _ := template.ParseFiles("Templates/index.html")
+    t.Execute(w, nil)
 }
 
 func Hello(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
     fmt.Fprintf(w, "hello, %s!\n", ps.ByName("name"))
 }
 
+func Edit(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+    p, err := loadPage("log")
+    if err != nil {
+        log.Println("log.txt not found")
+        p = &Page{Title: "log"}
+    }
+    t, _ := template.ParseFiles("Templates/edit.html")
+    t.Execute(w, p)
+}
+
+func Save(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+    log.Println(r.PostFormValue("body"))
+    p, err := loadPage("log")
+    if err != nil {
+        return
+    }
+    p.Body = []byte(r.PostFormValue("body"))
+    p.save()
+    http.Redirect(w, r, "/", http.StatusFound)
+}
+
 func main() {
     router := httprouter.New()
     router.GET("/", Index)
     router.GET("/hello/:name", Hello)
+    router.GET("/edit/", Edit)
+    router.POST("/save/", Save)
 
     log.Fatal(http.ListenAndServe(":8080", router))
 }
