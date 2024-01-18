3d905a98599319dd86485ee0f2a19e53a6478386
Max <max.nibler@gmail.com>
2024-01-18 20:06:43 +0900

    Basic edit and load using htmx

--- homesite.go
+++ homesite.go
@@ -9,31 +9,35 @@ import (
     "github.com/julienschmidt/httprouter"
 )
 
-type Block struct {
+type Blurb struct {
     Title string
     Body []byte
 }
 
-func (b *Block) save() error {
+func (b *Blurb) save() error {
     return os.WriteFile(b.path(), b.Body, 0600)
 }
 
-func (b *Block) path() string {
-    return "Blocks/" + b.Title + ".txt"
+func (b *Blurb) path() string {
+    return "Blurbs/" + b.Title + ".txt"
 }
 
-func loadBlock(title string) (*Block, error) {
-    filename := "Blocks/" + title + ".txt"
+func loadBlurb(title string) (*Blurb, error) {
+    filename := "Blurbs/" + title + ".txt"
     body, err := os.ReadFile(filename)
     if err != nil {
         return nil, err
     }
-    return &Block{Title: title, Body: body}, nil
+    return &Blurb{Title: title, Body: body}, nil
 }
 
-func renderTemplate(w http.ResponseWriter, tmpl string, p *Block) {
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Blurb) {
     t, _ := template.ParseFiles("Templates/" + tmpl + ".html")
     t.Execute(w, p)
+}
+
+func templatePath(name string) string {
+    return "Templates/" + name + ".html"
 }
 
 func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
@@ -42,23 +46,35 @@ func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 
 func Edit(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
     title := ps.ByName("title")
-    b, err := loadBlock(title)
+    b, err := loadBlurb(title)
     if err != nil {
         log.Println(title + ".txt not found")
-        b = &Block{Title: "log"}
+        b = &Blurb{Title: "log"}
     }
     renderTemplate(w, "edit", b)
 }
 
 func Save(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
     title := ps.ByName("title")
-    b, err := loadBlock(title)
+    b, err := loadBlurb(title)
     if err != nil {
         return
     }
     b.Body = []byte(r.PostFormValue("body"))
     b.save()
-    http.Redirect(w, r, "/", http.StatusFound)
+    tmpl, _ := template.ParseFiles(templatePath("blurb"))
+    tmpl.Execute(w, b)
+}
+
+func GetBlurb(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+    title := ps.ByName("title")
+    b, err := loadBlurb(title)
+    if err != nil {
+        log.Println(title + ".txt not found")
+        b = &Blurb{Title: title}
+    }
+    tmpl, _ := template.ParseFiles(templatePath("blurb"))
+    tmpl.Execute(w, b)
 }
 
 func main() {
@@ -66,6 +82,7 @@ func main() {
     router.GET("/", Index)
     router.GET("/edit/:title", Edit)
     router.POST("/save/:title", Save)
+    router.GET("/blurb/:title", GetBlurb)
 
     log.Fatal(http.ListenAndServe(":8080", router))
 }
